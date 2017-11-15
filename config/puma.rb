c8c6b8532504628007e3c0a766313b9d5706f828
Simeon123 <semen@sequenia.com>
2017-11-15 22:25:56 +0700

    Настройка puma (сервер стартует)

--- config/puma.rb
+++ config/puma.rb
@@ -9,12 +9,15 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum, this matches the default thread size of Active Record.
 #
-threads_count = ENV["RAILS_MAX_THREADS"] || 5
+require 'dotenv'
+Dotenv.load
 
+threads_count = ENV["RAILS_MAX_THREADS"]
 threads threads_count, threads_count
-port        ENV['RAILS_ENV'] || 'production'
-workers     ENV["WEB_CONCURRENCY"] || 2
-environment ENV['RAILS_ENV'] || 'production'
+
+port        ENV["PORT"]
+workers     ENV["WEB_CONCURRENCY"]
+environment ENV["RAILS_ENV"]
 
 cwd = File.dirname(__FILE__) + '/..'
 
@@ -23,11 +26,11 @@ rackup "#{cwd}/config.ru"
 
 pidfile "#{cwd}/tmp/pids/puma.pid"
 
-bind 'tcp://0.0.0.0:8084'
+# bind "tcp://0.0.0.0:#{ENV["PORT"]}"
 bind "unix://#{cwd}/tmp/sockets/slovaris_puma.sock"
 
 on_worker_boot do
-    ActiveSupport.on_load(:active_record) do
-        ActiveRecord::Base.establish_connection
-    end
+    # ActiveSupport.on_load(:active_record) do
+    #     ActiveRecord::Base.establish_connection
+    # end
 end
